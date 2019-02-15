b691e4fa45f0a116b65a2fe7ee7440132f84e1c9
faridullah_codegini <faridullah996@gmail.com>
2019-02-15 12:45:30 +0500

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,47 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'timer';
+  timeLeft;
+  hoursLeft;
+  minutesLeft;
+  secondsLeft;
+  interval;
+  constructor() {
+    let currentTime = new Date();
+        let acceptingTime = new Date(2019,1,15,12,25,0);
+        let arrivalTime = new Date(acceptingTime.getTime());
+        this.timeLeft = arrivalTime.getTime() - currentTime.getTime();
+
+        if (this.timeLeft > 0) {
+          this.hoursLeft = Math.round((this.timeLeft / (1000 * 60 * 60)) % 24);
+          this.minutesLeft = Math.round((this.timeLeft / 60000) % 60)
+          this.secondsLeft = Math.round((this.timeLeft / 1000) % 60)
+
+          this.startTimer();
+        }else{
+          this.hoursLeft = 0;
+          this.minutesLeft = 0;
+          this.secondsLeft = 0;
+        }
+
+  }
+  
+  startTimer() {
+    setInterval(() => {
+      if(this.secondsLeft > 0) {
+        this.secondsLeft--;
+      }else{
+        if (this.minutesLeft < 0 && this.hoursLeft > 0){
+            this.minutesLeft = 60;
+            this.hoursLeft--;
+        }
+
+        if (this.minutesLeft >= 0)
+          this.secondsLeft = 60;
+
+        this.minutesLeft--;
+      }
+    },1000)
+  }
+
 }
